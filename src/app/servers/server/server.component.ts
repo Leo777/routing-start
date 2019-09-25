46539d90cd430774bef5924295baad338fe9ba14
Lev <borichevskiy@gmail.com>
2019-09-25 22:22:06 +0200

    Added routing and child components

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
@@ -10,7 +10,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute, 
+    private router: Router) { }
 
   ngOnInit() {
    const id = +this.route.snapshot.params['id'];
@@ -19,6 +22,10 @@ export class ServerComponent implements OnInit {
       this.server =this.serversService.getServer(+params['id']);
     })
 
+  }
+
+  onEdit(){
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling:'preserve'})
   }
 
 }
